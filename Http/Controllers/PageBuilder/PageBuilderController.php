10cd03398eef2a4d076c29f693b0b4359c3742ad
Hafijul Islam <hafijul.islam@easyask.com>
2025-07-12 06:24:50 +0600

    Update PageBuilderController.php

--- Http/Controllers/PageBuilder/PageBuilderController.php
+++ Http/Controllers/PageBuilder/PageBuilderController.php
@@ -1,1 +1,13 @@
+<?php
 
+namespace Amplify\System\Cms\Http\Controllers\PageBuilder;
+
+use App\Http\Controllers\Controller;
+
+class PageBuilderController extends Controller
+{
+    public function index()
+    {
+        return view('page-builder.index');
+    }
+}

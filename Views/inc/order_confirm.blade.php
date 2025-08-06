307e8d36994d89a114b08cb4008cdd0e632d008d
arifeasyask <ariful.islam@easyask.com>
2025-08-06 21:56:29 +0600

    :up: Update: => add all items confirm message

--- Views/inc/order_confirm.blade.php
+++ Views/inc/order_confirm.blade.php
@@ -1,8 +1,8 @@
 <div class="modal fade" id="order-confirm" tabindex="-1" aria-labelledby="orderConfirm" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-sm">
+    <div class="modal-dialog modal-dialog-centered modal-md">
         <div class="modal-content shadow-sm">
                 <div class="modal-body">
-                    <h3 class="text-center">{{ __('Are you sure?') }}</h3>
+                    <h3 class="text-center">{{ __('Your current cart will be cleared and replaced with these items. Do you want to continue?') }}</h3>
                 </div>
                 <div class="modal-footer justify-content-around pt-0 border-top-0">
                     <button type="button" data-dismiss="modal" class="btn btn-dark">{{ __('No') }}</button>

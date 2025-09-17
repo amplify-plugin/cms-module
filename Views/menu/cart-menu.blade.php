cd17b0a66769dfebb4385e07b7564e4aa7ce5c9c
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-09-17 21:10:18 +0600

    mix manifest added

--- Views/menu/cart-menu.blade.php
+++ Views/menu/cart-menu.blade.php
@@ -7,21 +7,26 @@
     <div class="toolbar-dropdown">
         <span class="text-dark toolbar-close"><i class="icon-cross"></i></span>
         <p class="cart-dropdown-title">Cart Items (<span class="total_cart_items">0</span>)</p>
-
         <div class="cart-dropdown">
         </div>
-
-        <div class="toolbar-dropdown-group">
+        <div class="toolbar-dropdown-group text-dark text-black">
             <div class="column"><span class="text-lg">Total:</span></div>
-            <div class="column text-right"><span class="text-lg text-medium total_cart_amount">$0.00</span>
+            <div class="column text-right">
+                <span class="text-lg text-medium total_cart_amount">$0.00</span>
             </div>
         </div>
         <div class="toolbar-dropdown-group">
-            <div class="column"><a class="btn btn-sm btn-block btn-secondary"
-                                   href="{{ route('frontend.carts.index') }}">View Cart</a>
+            <div class="column">
+                <a class="btn btn-sm btn-block btn-secondary"
+                   href="{{ route('frontend.carts.index') }}">
+                    View Cart
+                </a>
             </div>
-            <div class="column"><a class="btn btn-sm btn-block btn-success"
-                                   href="{{ route('frontend.checkout') }}">Checkout</a>
+            <div class="column">
+                <a class="btn btn-sm btn-block btn-success"
+                   href="{{ route('frontend.checkout') }}">
+                    Checkout
+                </a>
             </div>
         </div>
     </div>

dc58a4a6b5f7194e00a1dc9e472d642b1d05e717
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-11-04 14:27:20 +0600

    logout JS error issue fixed

--- Views/menu/account-menu/logout.blade.php
+++ Views/menu/account-menu/logout.blade.php
@@ -1,7 +1,9 @@
-<li class="sub-menu-separator"></li>
-<li>
-    <a href="#" onclick="accountLogout(event, this);" data-route="{{ route('frontend.logout') }}">
+<li class="sub-menu-separator">
+    <a href="#" onclick="document.getElementById('account-logout-form').submit(); return false;">
         <i class="icon-unlock"></i>
         Logout
     </a>
 </li>
+<form action="{{ route('frontend.logout') }}" method="POST" id="account-logout-form">
+    @csrf
+</form>

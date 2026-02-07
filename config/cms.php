57dc8d826e3c2ee1490ef1ca360b8ffc08d4e67b
Mohammad Hafijul Islam <hafijul233@gmail.com>
2026-02-08 01:59:38 +0600

    :pencil: update config removed mw default values

--- config/cms.php
+++ config/cms.php
@@ -11,11 +11,10 @@
 return [
 
     'default' => env('DEFAULT_TEMPLATE', 'template-1'),
-    'logo_path' => 'https://mountainwest.s3.amazonaws.com/images/banners/car-audio-banner-full.jpeg',
-    'favicon_path' => 'https://mountainwest.s3.amazonaws.com/images/banners/car-audio-banner-full.jpeg',
+    'logo_path' => '',
+    'favicon_path' => '',
     'email' => null,
     'phone' => null,
-    'header_content' => null,
     'slogan' => 'Amplify Your Online Business',
     'favicon' => '',
     'fallback' => 'demo',

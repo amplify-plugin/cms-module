de25c83db1ace136a78170f6ac65b1271a8585e9
Mohammad Hafijul Islam <hafijul233@gmail.com>
2026-04-12 22:20:49 +0600

    :pencil2: ACP => Hide subtotal and quantity count when there is no item into the cart and quantity get updated upon a user interaction

--- resources/views/menu/cart-menu.blade.php
+++ resources/views/menu/cart-menu.blade.php
@@ -1,20 +1,20 @@
 <div id="show-cart" class="cart position-relative flex-shrink-0" {!! $htmlAttributes !!}>
     <i class="pe-7s-cart font-weight-bolder"></i>
     @if($showBadge)
-        <span @class(["cart-badge badge total_cart_items", 'd-none' => empty($itemCount)]) >0</span>
+        <span @class(["cart-badge badge total_cart_items", 'd-none' => empty($itemCount)]) >{{$itemCount}}</span>
     @endif
-    <span @class(["count total_cart_items", 'd-none' => empty($itemCount)]) >0</span>
+    <span @class(["count total_cart_items", 'd-none' => empty($itemCount)]) >{{$itemCount}}</span>
     <div class="toolbar-dropdown">
         <span class="text-dark toolbar-close"><i class="icon-cross"></i></span>
-        <p class="cart-dropdown-title">Cart Items (<span class="total_cart_items">0</span>)</p>
+        <p class="cart-dropdown-title">{{ __('Cart Items') }} (<span class="total_cart_items">0</span>)</p>
         <div class="cart-dropdown">
         </div>
         <div class="toolbar-dropdown-group" id="cart-menu-subtotal" style="display: none;">
             <div class="column">
-                <span class="text-lg">Subtotal:</span>
+                <span class="text-lg">{{ __('Subtotal:') }}</span>
             </div>
             <div class="column text-right">
-                <span class="text-lg text-medium total_cart_amount">$0.00</span>
+                <span class="text-lg text-medium total_cart_amount">{{ currency_format(value:'0.00', withSymbol: true) }}</span>
             </div>
         </div>
         <div class="toolbar-dropdown-group">
